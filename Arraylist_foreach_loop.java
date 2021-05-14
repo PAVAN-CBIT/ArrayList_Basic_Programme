d9185016136ea539e13fff92374a53fdc48b361c
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-05-14 09:03:43 +0530

    Create Arraylist_foreach_loop.java

---
+++ Arraylist_foreach_loop.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		ArrayList<String>animals = new ArrayList<String>();
+		animals.add("cat");
+		animals.add("dog");
+		animals.add("lion");
+		animals.add("tiger");
+		
+		
+	    	for(String value:animals)
+	    	{
+	    	    
+	    	    System.out.println(value);
+	    	}
+		
+	}
+}
