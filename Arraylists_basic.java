72a882859176495d0438ed578fd3205b66c05e26
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-05-21 08:16:51 +0530

    Create Arraylists_basic.java

---
+++ Arraylists_basic.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class Main
+{
+
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		ArrayList <String>words =new<String> ArrayList();
+		words.add("Hello");
+		words.add("there");
+		words.add("10");
+		words.add("9");
+		String item1=words.get(2);
+		String item2=words.get(3);
+		System.out.println(item1);
+	}
+}
